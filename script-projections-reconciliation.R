cb712abebcb3d9173017221f57696763c2eee1e6
Griffith Feeney <feeney@gfeeney.com>
2021-12-13 19:54:17 -0500

    Renames for clarity
    proj for projection, projLM for list matrix of multiple projections

--- script-projections-reconciliation.R
+++ script-projections-reconciliation.R
@@ -1,38 +1,39 @@
 rm(list = ls())
 metadata <- readRDS("metadata.rds")
+source(paste0(metadata$paths$path2R, "projection.R"))
 path2inputs <- metadata$paths$path2inputs
 path2outputs <- metadata$paths$path2outputs
 path2R <- metadata$paths$path2R
-source(paste0(path2R, "projection.R"))
 places  <- metadata$place$places
 pcycles <- metadata$time$pcycles
 BaseASD <- readRDS(paste0(path2inputs, "BaseASD/BaseASD.rds"))  # View(BaseASD)
 nLx     <- readRDS(paste0(path2inputs, "nLx/nLx.rds"))
 ASBR    <- readRDS(paste0(path2inputs, "ASBR/ASBR.rds"))
-NIMR   <- readRDS(paste0(path2inputs, "NIMR/NIMRd.rds"))
+NIMR   <- readRDS(paste0(path2inputs, "NIMR/NIMRd.rds"))  # Note NIMRs!!!
 
-projections <- vector(mode = "list", length = length(places))
-names(projections) <- places
-projections.init <- projections
-for (i in 1:length(projections)) {
-  place <- names(projections)[i]
-  projections.init[[i]] <- 
-    initialize.projection.pframes(place, BaseASD, nLx, ASBR, NIMR)
-  projections[[i]] <- calculate.projection(projections.init[[i]])
+proj5LM <- vector(mode = "list", length = length(places))
+names(proj5LM) <- places
+proj5LM.init <- proj5LM
+for (i in 1:length(proj5LM)) {
+  place <- names(proj5LM)[i]
+  proj5LM.init[[i]] <- initialize.projection.pframes(place, BaseASD, nLx, ASBR, NIMR)
+  proj5LM[[i]] <- calculate.projection(proj5LM.init[[i]])
 }
 
-
-# National Projection
-np <- projections$Kenya
-cols <- c("ASDin", "Deaths", "Births", "Survivors", "ASDout")
-for (i in 1:length(np)) {
-  np[[i]][, cols] <- round(np[[i]][, cols], 0)
-}
-np  # These are the numbers we want subnational projections to be consistent with
+# National Projection I'M NOT SURE ABOUT THIS NOW, HOLD OFF
+np <- proj5LM$Kenya  # national
+# cols <- c("ASDin", "Deaths", "Births", "Survivors", "ASDout")
+# for (i in 1:length(np)) {
+#   np[[i]][, cols] <- round(np[[i]][, cols], 0)
+# }
+# np  # These are the numbers we want subnational projections to be consistent with
 
 # Consistent subnational projections
-snpi <- transpose.ListMatrix(projections.init[2:48])  # This needs explaining
+snpi <- transpose.ListMatrix(proj5LM.init[2:48])  # This needs explaining
 snp <- calculate.consistent.projections(np, snpi)
+
+# SO NOW I WANT TO PUT np and snp BACK TOGETHER TO GET A CONSISTENT proj5LM
+# STOP HERE, TOO TIRED TO DO MORE TONIGHT, WILL JUST SCREW IT UP
 
 # Consistency parameters
 rMatrixList <- vector(mode = "list", length = length(snp))
@@ -44,10 +45,10 @@ lapply(rMatrixList, round, 3)
 
 # Look at county population change
 snp <- transpose.ListMatrix(snp)  # This needs explaining
-projectionTotals <- t(sapply(snp, get.projTotals))
-projectionTotals <- sort.PlacesByGrowth(projectionTotals)
-projectionTotals
-write.csv(projectionTotals, paste0(path2outputs, "projections/projectionTotals.csv"))
+proj5Totals <- t(sapply(snp, get.projTotals))
+proj5Totals <- sort.PlacesByGrowth(proj5Totals)
+proj5Totals  # This is fine, we just don't have Kenya at top
+write.csv(proj5Totals, paste0(path2outputs, "projections/proj5Totals.csv"))
 
 # Look at components of change for counties
 get.ComponentsMatrix(snp$Kwale)
--- script-projections.R
+++ script-projections.R
@@ -1,9 +1,8 @@
 # PREPARATORY STEPS
 metadata <- readRDS("metadata.rds")
-source(paste0(path2R, "projection.R"))
+source(paste0(metadata$paths$path2R, "projection.R"))
 path2inputs <- metadata$paths$path2inputs
 path2outputs <- metadata$paths$path2outputs
-path2R <- metadata$paths$path2R
 places  <- metadata$place$places
 pcycles <- metadata$time$pcycles
 place <- places[1]
@@ -15,44 +14,46 @@ NIMRb <- readRDS(paste0(path2inputs, "NIMR/NIMRb.rds"))  # Migration raw
 NIMRc <- readRDS(paste0(path2inputs, "NIMR/NIMRc.rds"))  # Migration adjusted
 NIMRd <- readRDS(paste0(path2inputs, "NIMR/NIMRd.rds"))  # Add decline
 
-# NEED TO REWRITE using 'projection' and 'projectionsList' ?? prjs4places listof.projections4places argh!
+# NEED TO REWRITE using 'projection' and 'projectionsList'
 
 # PROJECTION 1: No migration
 NIMR <- NIMRa
-proj1 <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
-saveRDS(proj1, paste0(path2outputs, "projections/proj1.rds"))
-proj1Totals <- t(sapply(proj1, get.projTotals))
+proj1LM <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
+saveRDS(proj1LM, paste0(path2outputs, "projections/proj1LM.rds"))
+proj1Totals <- t(sapply(proj1LM, get.projTotals))
 proj1Totals <- sort.PlacesByGrowth(proj1Totals)
 proj1Totals
 write.csv(proj1Totals, paste0(path2outputs, "projections/proj1Totals.csv"))
 
 # PROJECTION 2: Migration raw
 NIMR   <- readRDS(paste0(path2inputs, "NIMR/NIMRb.rds"))
-proj2 <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
-saveRDS(proj2, paste0(path2outputs, "projections/proj2.rds"))
-proj2Totals <- t(sapply(proj2, get.projTotals))
+proj2LM <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
+saveRDS(proj2LM, paste0(path2outputs, "projections/proj2LM.rds"))
+proj2Totals <- t(sapply(proj2LM, get.projTotals))
 proj2Totals <- sort.PlacesByGrowth(proj2Totals)
 proj2Totals
 write.csv(proj2Totals, paste0(path2outputs, "projections/proj2Totals.csv"))
 
 # PROJECTION 3: Migration adjusted by LTCSRTMig census survival estimates
 NIMR  <- readRDS(paste0(path2inputs, "NIMR/NIMRc.rds"))
-proj3 <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
-saveRDS(proj3, paste0(path2outputs, "projections/proj3.rds"))
-proj3Totals <- t(sapply(proj3, get.projTotals))
+proj3LM <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
+saveRDS(proj3LM, paste0(path2outputs, "projections/proj3LM.rds"))
+proj3Totals <- t(sapply(proj3LM, get.projTotals))
 proj3Totals <- sort.PlacesByGrowth(proj3Totals)
 proj3Totals
 write.csv(proj3Totals, paste0(path2outputs, "projections/proj3Totals.csv"))
 
 # PROJECTION 4: Migration adjusted with decline
 NIMR  <- readRDS(paste0(path2inputs, "NIMR/NIMRd.rds"))
-proj4 <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
-saveRDS(proj4, paste0(path2outputs, "projections/proj4.rds"))
-proj4Totals <- t(sapply(proj4, get.projTotals))
+proj4LM <- calculate.projections(places, BaseASD, nLx, ASBR, NIMR, md = metadata)
+saveRDS(proj4LM, paste0(path2outputs, "projections/proj4LM.rds"))
+proj4Totals <- t(sapply(proj4LM, get.projTotals))
 proj4Totals <- sort.PlacesByGrowth(proj4Totals)
 proj4Totals
 write.csv(proj4Totals, paste0(path2outputs, "projections/proj4Totals.csv"))
 
+# PROJECTION 5: NIMRd with reconciliation of subnational nation to national projections
+# See script-projections-reconciliation.R
 
               
 
